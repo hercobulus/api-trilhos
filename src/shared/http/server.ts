4135a8406bf20fbe34f53eb6981dec4a55c52cd1
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-15 18:35:03 -0300

    add typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,22 +1,29 @@
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import logger from '../logger/logger';
-
+import createConnection from '../typeorm';
 import handlerError from '../middlewares/handlerError';
 
-const PORT = process.env.SERVER_PORT || 8000;
+createConnection()
+  .then(function () {
+    const PORT = process.env.SERVER_PORT || 8000;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ msg: 'Okay' });
-});
+    app.get('/', (request, response) => {
+      return response.json({ msg: 'Okay' });
+    });
 
-app.use(handlerError);
+    app.use(handlerError);
 
-app.listen(PORT, function () {
-  logger(`Server started on port: ${PORT}`);
-});
+    app.listen(PORT, function () {
+      logger(`Server started on port: ${PORT}`);
+    });
+  })
+  .catch(function (e) {
+    logger(e);
+  });
--- src/shared/logger/logger.ts
+++ src/shared/logger/logger.ts
@@ -1,3 +1,4 @@
 export default function (message: string, time = new Date()) {
-  return `${time} - ${message}`;
+  // eslint-disable-next-line no-console
+  console.log(`${time} - ${message}`);
 }
